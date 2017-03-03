f5eec20bcc0282b0b1e655d5b013856c3bf891e4
Oliver Wang <48oliverwang48@gmail.com>
2017-03-02 22:10:20 -0800

    Added run broker button functionality

--- Fantasia/app/src/main/java/com/oliverwang/fantasia/MainActivity.java
+++ Fantasia/app/src/main/java/com/oliverwang/fantasia/MainActivity.java
@@ -1,18 +1,27 @@
 package com.oliverwang.fantasia;
 
+import android.Manifest;
 import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.view.ContextThemeWrapper;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import org.eclipse.moquette.server.Server;
+
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
@@ -48,12 +57,81 @@ public class MainActivity extends AppCompatActivity {
         verboseLog.setAdapter(logAdapter);
     }
 
+    public boolean isPermissionGranted() {
+        boolean writeExternalStorage = false;
+        boolean accessFineLocation = false;
+        if (Build.VERSION.SDK_INT >= 23) {
+            Log.v("permissions", "need runtime dialog");
+            if (checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                Log.v("external storage", "permission is granted");
+                writeExternalStorage = true;
+            } else {
+                Log.v("external storage", "Permission is revoked");
+                writeExternalStorage = false;
+                // ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+            }
+            if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                Log.v("fine location", "permission is granted");
+                accessFineLocation = true;
+            } else {
+                Log.v("fine location", "Permission is revoked");
+                accessFineLocation = false;
+                // ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+            }
+
+            if (writeExternalStorage == false && accessFineLocation == false) {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+                return false;
+            } else if (writeExternalStorage == false && accessFineLocation == true) {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+                return false;
+            } else if (writeExternalStorage == true && accessFineLocation == false) {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+                return false;
+            } else if (writeExternalStorage == true && accessFineLocation == true) {
+                return true;
+            } else return false;
+        }
+        //permission is automatically granted on any sdk lower than 23 upon installation
+        Log.v("all perms", "Permission is granted");
+        return true;
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        for (int i = 0; i < grantResults.length - 1; i++) {
+            if (grantResults[i] == PackageManager.PERMISSION_GRANTED) {
+                if (permissions[i].equals(Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                    Toast.makeText(getApplicationContext(), "Broker permissions added", Toast.LENGTH_SHORT).show();
+                    /*
+                    //run broker
+                    try {
+                        new Server().startServer();
+                        Toast.makeText(getApplicationContext(),"Broker started",Toast.LENGTH_LONG).show();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                        Log.v("launch broker","io exception");
+                    }
+                    */
+                } else if (permissions[i].equals(Manifest.permission.ACCESS_FINE_LOCATION)) {
+                    Toast.makeText(getApplicationContext(), "Geofencing permissions added", Toast.LENGTH_SHORT).show();
+                    /*
+                    Toast.makeText(getApplicationContext(),"Geofencing Permissions Added",Toast.LENGTH_LONG).show();
+                    */
+
+                }
+            }
+        }
+    }
+
     //@todo add custom alert dialogs for configureBroker, runBroker, configureClient, and runClient
     private View.OnClickListener buttonOnClickListener = new View.OnClickListener() {
         @Override
         public void onClick(final View v) {
 
-            switch(v.getId()){
+            switch (v.getId()) {
 
                 case R.id.configure_broker:
                     LayoutInflater inflater = getLayoutInflater();
@@ -81,7 +159,16 @@ public class MainActivity extends AppCompatActivity {
                     break;
 
                 case R.id.run_broker:
-
+                    if (isPermissionGranted()) {
+                        //run broker
+                        try {
+                            new Server().startServer();
+                            Toast.makeText(getApplicationContext(), "Broker started", Toast.LENGTH_SHORT).show();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                            Log.v("launch broker", "io exception");
+                        }
+                    }
                     break;
 
                 case R.id.configure_client:
